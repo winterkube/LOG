1012a4394908803b2abeb4e24ad613fd3fb1d38d
winterkube <theicekubex@gmail.com>
2024-10-05 05:07:00 -0700

    meth

--- src/components/games/HowToMath/Menu.js
+++ src/components/games/HowToMath/Menu.js
@@ -4,24 +4,31 @@ import React from 'react';
 import './styles/Menu.css';
 import playButtonImage from './assets/play button.png';
 import settingsButtonImage from './assets/settings button.png';
+import cheebaImage from './assets/dogss_spsh.png';
 
 function Menu({ onStart, onSettings }) {
     return (
         <>
+
+            <div className="cheeba"></div>
+
+
             <img
                 src={playButtonImage}
                 alt="Play Button"
                 className="play-button"
                 onClick={onStart}
-                width="300"
+                width="260"
             />
             <img
                 src={settingsButtonImage}
                 alt="Settings Button"
                 className="settings-button"
                 onClick={onSettings}
-                width="300"
+                width="260"
             />
+
+
         </>
     );
 }
